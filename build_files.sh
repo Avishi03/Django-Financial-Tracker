88203643503982f85e48bb42bf58510c74d085f9
Avishi03 <avishiagrawal0309@gmail.com>
2026-03-31 01:00:05 +0530

    Fix Vercel PIP externally managed environment error

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo "Installing pip..."
-python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip --break-system-packages || true
 echo "Installing requirements..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 echo "Running collectstatic..."
 python3 djfintracker/manage.py collectstatic --noinput
